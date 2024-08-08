6b66254c2b0083374421a6381133f9355e74612b
avayyyyyyy <jabhi465@gmail.com>
2024-08-08 11:41:25 +0530

    chore(app): remove unused imports and add favicon links BREAKING CHANGE: Removed unused imports for navigation and auth, added favicon links to the HTML head section. Removed next.svg, placeholder-user.jpg, and vercel.svg files.

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,6 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { SessionProvider } from "next-auth/react";
-import { redirect } from "next/navigation";
-import { auth } from "@/lib/auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +21,26 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className={`${inter.className}`}>
         <SessionProvider>
           <ThemeProvider
