65dee24cb07b64b625c23dd125eced10d12cb474
avayyyyyyy <jabhi465@gmail.com>
2024-09-22 17:07:16 +0530

    style: comment out unused span element in Announcement component

--- components/Announcement.tsx
+++ components/Announcement.tsx
@@ -5,7 +5,7 @@ const Announcement = () => {
     <>
       <div className="bg-zinc-900 px-4 py-3 text-zinc-100">
         <div className="text-center flex items-center justify-center md:text-sm text-xs font-medium">
-          <span className="font-bold md:block hidden">Update:</span>
+          {/* <span className="font-bold md:block hidden">Update:</span> */}
           <div className="inline-block ml-2">
             Congratulations, everyone! We’ve added over 10 new themes to
             customize your feedback form. 🎉
